e3c5e2ea3f4cc0252e251e07855679d6b5ef0e02
Veronika <shagiyanovavn@gmail.ru>
2024-02-20 01:49:51 +0300

    Add utils database 2

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+from classes.hh_parser import HHParser
 from config import config
 
 
@@ -36,3 +37,28 @@ def create_tables(db_name):
                         ')')
     conn.close()
 
+
+def insert_data_info_tables(db_name):
+    hh = HHParser()
+    employers = hh.get_employers()
+    vacancies = hh.filter_vacancies()
+    conn = psycopg2.connect(dbname=db_name, **config())
+    with conn:
+        with conn.cursor() as cur:
+            for employer in employers:
+                cur.execute('INSERT INTO employers VALUES (%s, %s)'
+                            'ON CONFLICT (id) DO NOTHING;',
+                            (employer['id'], employer['name']))
+
+            for vacancy in vacancies:
+                cur.execute('INSERT INTO vacancies VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'
+                            'ON CONFLICT (id) DO NOTHING;',
+                            (vacancy['id'],
+                             vacancy['name'],
+                             vacancy['area'],
+                             vacancy['salary_from'],
+                             vacancy['salary_to'],
+                             vacancy['published_at'],
+                             vacancy['url'],
+                             vacancy['employer']))
+    conn.close()
